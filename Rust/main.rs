d536cc392e6cb2ef99da35ad3e0c7b4c61842b82
bananaconcat <161962282+bananaconcat@users.noreply.github.com>
2025-04-26 01:52:02 +0100

    Update main.rs

--- Rust/main.rs
+++ Rust/main.rs
@@ -4,10 +4,7 @@ use std::str;
 #[derive(Copy, Clone)]
 enum TokenType
 {
-	Identifier,
-	Symbol,
-	DataType,
-	Keyword
+
 }
 
 
@@ -16,56 +13,27 @@ enum TokenType
 pub struct Token<'a>
 {
 	token_type: TokenType,
-	literal: &'a str
+	literal: &'a str,
+
+	c0: usize,
+	l0: usize,
+
+	l1: usize,
+	c1: usize
 }
 
-pub fn tokenize(source_code: &str) -> Vec<Token>
+pub struct Lexer<'a>
 {
-	let mut token_sets: Vec<Token> = Vec::new();
-	let mut tokens: Vec<Token> = Vec::new();
+	tokens: Vec<Token<'a>>,
+	cursor: i32
+}
 
-	token_sets.push(Token{token_type: TokenType::Symbol, literal: "("});
-	token_sets.push(Token{token_type: TokenType::Symbol, literal: ")"});
-	token_sets.push(Token{token_type: TokenType::Symbol, literal: "{"});
-	token_sets.push(Token{token_type: TokenType::Symbol, literal: "}"});
-	token_sets.push(Token{token_type: TokenType::Symbol, literal: ";"});
-	token_sets.push(Token{token_type: TokenType::DataType, literal: "i8"});
-	token_sets.push(Token{token_type: TokenType::DataType, literal: "i16"});
-	token_sets.push(Token{token_type: TokenType::DataType, literal: "i32"});
-	token_sets.push(Token{token_type: TokenType::DataType, literal: "i64"});
-	token_sets.push(Token{token_type: TokenType::DataType, literal: "str"});
-	token_sets.push(Token{token_type: TokenType::Keyword, literal: "return"});
+fn parse_next_token()
+{
 
-	let mut i: usize = 0;
-	let mut i_2: usize = 0;
+}
 
-	while i < source_code.len().try_into().expect("Failed to convert source code len from usize to i32.") {
-		let mut current_byte: Vec<u8> = Vec::new();
-		current_byte.push(source_code.as_bytes()[i]);
-		let current_char = str::from_utf8(&current_byte).expect("Failed to convert byte to string.");
-
-		println!("{}", current_char);
-
-		while i_2 < token_sets.len()
-		{
-			if token_sets[i_2].literal == current_char
-			{
-				tokens.push(token_sets[i_2]);
-			}
-
-			i_2 += 1;
-		}
-
-		i_2 = 0;
-		i += 1;
-	}
-
-	while i_2 < token_sets.len()
-	{
-		println!("{:?} {}", tokens[i_2].token_type, tokens[i_2].literal);
-
-		i_2 += 1;
-	}
-
-	return tokens;
+fn create_lexer()
+{
+	
 }
